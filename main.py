cf8940701ed39f66fae0f0e678ffbafb052a8160
Ilkin777dev <kerimliilkin961@gmail.com>
2026-02-17 15:28:06 +0400

    Added multipale page parsing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scraper.job_scraper import scrape_jobs
 
 role = input("Введите роль для поиска вакансий: ")
-scrape_jobs(role)
+pages = input("Сколько страниц нужно запарсить: ")
+scrape_jobs(role, pages)
--- scraper/job_scraper.py
+++ scraper/job_scraper.py
@@ -3,60 +3,54 @@ from bs4 import BeautifulSoup
 import csv
 
 
-def scrape_jobs(role):
-    """
-    Минимальный пример парсера вакансий по роли.
-    """
-    # заменяем пробелы на + для URL
+def scrape_jobs(role, pages):
     role_query = role.replace(" ", "+")
+    pages_query = int(pages.replace(" ", "+"))
 
-    # тестовый URL (позже заменим на реальный сайт)
-    url = f"https://hh.ru/search/vacancy?text={role_query}"
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
 
-    # Создаем нашего User Агента чтобы проходить блокировку и получать нужную страницу при запросе
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36"}    
+    all_jobs = []
 
-    # отправляем HTTP запрос
-    response = requests.get(url, headers = headers)
-    # print(response.text)
+    # цикл по страницам
+    for page in range(pages_query):
+        print(f"Парсим страницу {page+1}...")
 
-    # проверяем успешность запроса
-    if response.status_code != 200:
-        print("Ошибка запроса:", response.status_code)
-        return
+        url = f"https://hh.ru/search/vacancy?text={role_query}&page={page}"
+        response = requests.get(url, headers=headers)
 
-    # превращаем HTML текст в объект BeautifulSoup
-    soup = BeautifulSoup(response.text, "html.parser")
+        if response.status_code != 200:
+            print("Ошибка запроса:", response.status_code)
+            continue
 
-    # ищем вакансии (пример структуры сайта)
-    jobs = soup.find_all("div", attrs={"data-qa": "vacancy-serp__vacancy"})
-    # print("Found jobs:", len(jobs))
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    # записываем данные в CSV
-    with open("jobs.csv", "w", newline="", encoding="utf-8") as f:
-        writer = csv.writer(f)
-        writer.writerow(["Title", "Salary", "Company"])
+        jobs = soup.find_all("div", attrs={"data-qa": "vacancy-serp__vacancy"})
+        print("Found jobs:", len(jobs))
 
         for job in jobs:
             title = job.find("span", attrs={"data-qa": "serp-item__title-text"})
-            title = title.text.strip() if title else "None"
-
-            salary = "No salary"
-            for span in job.find_all("span"):
-                text = span.get_text()
-                if "₽" in text or "$" in text or "₼" in text:
-                    salary = text.strip()
-                    break
+            # data-qa="serp-item__title-text" Vacancies titles data-qa
+            title = title.text.strip() if title else "No title"
 
             company = job.find("span", attrs={"data-qa": "vacancy-serp__vacancy-employer-text"})
-            company = company.text.strip() if company else "None"
+            company = company.text.strip() if company else "No company"
 
-            writer.writerow([title, salary, company])
+            # поиск зарплаты
+            salary = "No salary"
+            spans = job.find_all("span")
+            for s in spans:
+                if "₽" in s.text or "$" in s.text or "€" in s.text:
+                    salary = s.text.strip()
+                    break
 
-            # title = job.text.strip()
-            # sallary = job.text
-            # writer.writerow([title, ""])
-            # desc = job.find("p").text.strip()
-            # writer.writerow([title, desc])
+            all_jobs.append([title, company, salary])
 
-    print(f"Найдено {len(jobs)} вакансий. Данные сохранены в jobs.csv")
+    # запись CSV
+    with open("jobs.csv", "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Title", "Company", "Salary"])
+        writer.writerows(all_jobs)
+
+    print(f"Сохранено {len(all_jobs)} вакансий")
